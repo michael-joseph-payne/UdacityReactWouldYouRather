e8ad0ff591c171a2711101760959383e84612a72
Mike Payne <michael.joseph.payne@gmail.com>
2021-10-10 04:57:46 +0000

    Add question stores new question in the store

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,39 @@
+import { _saveQuestion } from '../db/_DATA.js'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  }
+}
+
+export function addQuestion (question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+}
+
+export function handleAddQuestion (optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser, users } = getState();
+    
+    const userData = Object.entries(users).map(([ key, user ]) => {
+      return user;
+	});
+    
+    const authorData = userData.find((user) => {
+      return user.name === authedUser;
+    });
+
+    return _saveQuestion({
+      optionOneText: optionOneText,
+      optionTwoText: optionTwoText,
+      author: authorData.id,
+    })
+      .then((question) => dispatch(addQuestion(question)))
   }
 }
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { handleAddQuestion } from '../actions/questions.js'
 
 class NewQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question1: null,
+      question2: null,
+    };
+    
+    this.handleQuestion1Change = this.handleQuestion1Change.bind(this);
+    this.handleQuestion2Change = this.handleQuestion2Change.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleQuestion1Change(event) {
+    this.setState({
+      question1: event.target.value,
+    });
+  }
+  
+    handleQuestion2Change(event) {
+    this.setState({
+      question2: event.target.value,
+    });
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    const { question1, question2 } = this.state;
+    const { dispatch } = this.props
+    
+    dispatch(handleAddQuestion(question1, question2))
+  }
+  
   render() {
     return (
       <div>
         <div>Create New Question</div>
         <div>Complete the question:</div>
         <div>Would you rather {'...'}</div>
-		<input type="text" />
+		<input type="text" onChange={ this.handleQuestion1Change } />
 		<div>OR</div>
-		<input type="text" />
+		<input type="text" onChange={ this.handleQuestion2Change } />
 		<div>
-		  <button>Submit</button>
+		  <button onClick={ this.handleSubmit } >Submit</button>
 		</div>
       </div>
     );
   }
 }
 
-export default NewQuestion;
+export default connect()(NewQuestion);
